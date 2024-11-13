0a41fea86a5c167c496e3a44b8d298fe8d99a6e7
Alex M <msxander@gmail.com>
2024-11-13 19:06:04 +0300

    Master init state

---
+++ src/main/java/com/example/demoenversaudited/DemoEnversAuditedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoenversaudited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEnversAuditedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEnversAuditedApplication.class, args);
+    }
+
+}
